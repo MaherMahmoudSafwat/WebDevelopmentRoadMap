0611af28ac8398b8687eedba4b7c79413f3f4c15
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2024-12-26 23:50:51 +0200

    Language Syntax

---
+++ IntroductionToCSharp/3-LanguageSyntax/LanguaugeSyntax/LanguaugeSyntax/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LanguaugeSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine and Special Characters \n are used to print the strings in to different lines
+            Console.WriteLine("Hello, World!\n");
+            Console.WriteLine("My Name is Maher");
+
+            //Console.write is used to write different strings and output it in the same line
+            Console.Write("Hello, World!");
+            Console.Write("Console.Write()");
+
+        }
+    }
+}
