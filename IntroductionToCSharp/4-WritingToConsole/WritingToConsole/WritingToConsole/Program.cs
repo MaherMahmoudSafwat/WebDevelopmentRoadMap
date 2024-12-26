50347436c03d4352a4e598b79d25c2ee7790d649
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2024-12-27 01:12:22 +0200

    Writing To Console

---
+++ IntroductionToCSharp/4-WritingToConsole/WritingToConsole/WritingToConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WritingToConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Title is used to change the title of the console window
+            Console.Title = "Writing to Console";
+            //Console.foregroundColor = ConsoleColor.Green is used to change the color of string
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+            //Console.backgroundColor = ConsoleColor.Yellow is used to change the color of background of the string
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("I Love C#");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadKey();
+        }
+    }
+}
