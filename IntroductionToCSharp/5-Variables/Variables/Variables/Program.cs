6d59cba3278a7bd2eae034b5467e252754c061bf
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2024-12-27 07:17:52 +0200

    Variables

---
+++ IntroductionToCSharp/5-Variables/Variables/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a variable
+            string name;
+            //Assign a value to the variable
+            name = "John";
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+        }
+    }
+}
