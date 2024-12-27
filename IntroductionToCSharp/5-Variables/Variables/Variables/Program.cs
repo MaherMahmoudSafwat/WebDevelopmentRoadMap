3e5a540de9830598345b484a30f78ed50a914ca9
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2024-12-27 17:43:12 +0200

    Update Program.cs

--- IntroductionToCSharp/5-Variables/Variables/Variables/Program.cs
+++ IntroductionToCSharp/5-Variables/Variables/Variables/Program.cs
@@ -13,6 +13,10 @@
             Console.WriteLine(name);
             Console.WriteLine(name);
             Console.WriteLine(name);
-        }
+            //Variables rules is same as C++.
+            //@ is used in C# to treat a string as a literal. It is useful when we want to use a reserved keyword as a variable name.
+            string @void = "A";
+            Console.WriteLine(@void);
+          }
     }
 }
