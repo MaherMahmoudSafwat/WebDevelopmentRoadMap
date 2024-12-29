eebf367fe097eaf4922095c3945861957f8fc1c3
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2024-12-29 13:42:15 +0200

    Update Program.cs

--- IntroductionToCSharp/15-ComparsionOperators/ComparsionOperators/ComparsionOperators/Program.cs
+++ IntroductionToCSharp/15-ComparsionOperators/ComparsionOperators/ComparsionOperators/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ComparsionOperators
+namespace ComparsionOperators
 {
     internal class Program
     {
@@ -14,18 +14,18 @@
              <= is used to compare if the left value is less than or equal to the right value
             */
             int a = 10;
-            Console.WriteLine($"a == 10 ? {a == 10}"); // True
-            Console.WriteLine($"a == 20 ? {a == 20}"); // False
-            Console.WriteLine($"a != 10 ? {a != 10}"); // False
-            Console.WriteLine($"a != 15 ? {a != 15}"); // True
-            Console.WriteLine($"a > 10 ? {a > 10}"); // False
-            Console.WriteLine($"a > 5 ? {a > 5}"); // True
-            Console.WriteLine($"a < 20 ? {a < 20}"); // True
-            Console.WriteLine($"a < 10 ? {a < 10}"); // False
-            Console.WriteLine($"a >= 10 ? {a >= 10}"); // True
-            Console.WriteLine($"a >= 15 ? {a >= 15}"); // False
-            Console.WriteLine($"a <= 10 ? {a <= 10}"); // True
-            Console.WriteLine($"a <= 2 ? {a <= 2}"); // False
+            Console.WriteLine($"a == 10 : {a == 10}"); // True
+            Console.WriteLine($"a == 20 : {a == 20}"); // False
+            Console.WriteLine($"a != 10 : {a != 10}"); // False
+            Console.WriteLine($"a != 15 : {a != 15}"); // True
+            Console.WriteLine($"a > 10 : {a > 10}"); // False
+            Console.WriteLine($"a > 5 : {a > 5}"); // True
+            Console.WriteLine($"a < 20 : {a < 20}"); // True
+            Console.WriteLine($"a < 10 : {a < 10}"); // False
+            Console.WriteLine($"a >= 10 : {a >= 10}"); // True
+            Console.WriteLine($"a >= 15 : {a >= 15}"); // False
+            Console.WriteLine($"a <= 10 : {a <= 10}"); // True
+            Console.WriteLine($"a <= 2 : {a <= 2}"); // False
         }
     }
 }
