d265c3f47906a66200ff1e9b1eb7943130929bc2
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2024-12-29 13:40:11 +0200

    Update Program.cs

--- IntroductionToCSharp/5-Variables/Variables/Variables/Program.cs
+++ IntroductionToCSharp/5-Variables/Variables/Variables/Program.cs
@@ -17,6 +17,11 @@
             //@ is used in C# to treat a string as a literal. It is useful when we want to use a reserved keyword as a variable name.
             string @void = "A";
             Console.WriteLine(@void);
+            //var is a keyword that is used to declare a variable and initialize it at the same time.
+            //The type of the variable is inferred by the compiler.
+            var a = 10;
+            Console.WriteLine($"a = {a}");
+            //a = "abc" ; //Error: Cannot implicitly convert type 'string' to 'int'
           }
     }
 }
