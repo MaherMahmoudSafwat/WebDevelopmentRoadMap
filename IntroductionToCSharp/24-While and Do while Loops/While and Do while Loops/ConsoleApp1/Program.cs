8a4c65e072239938adc3ccbf5159b3fe1fbf2e5b
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-02 22:43:29 +0200

    While and Do While Loops

---
+++ IntroductionToCSharp/24-While and Do while Loops/While and Do while Loops/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do while loop is used when we want to execute the block of code at least once and then check the condition.
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x <= 5);
+            //While loop is used when we want to execute the block of code only if the condition is true.
+            int y = 1;
+            while (y <= 5)
+            {
+                Console.WriteLine(y);
+                y++;
+            }
+            //infinite loop using while loop
+            while(true)
+            {
+                Console.WriteLine("Please enter a string to Capitalize or press Exit to exit :- ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    //break statement is used to exit the loop.
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(input.ToUpper());
+                }
+            }
+        }
+    }
+}
