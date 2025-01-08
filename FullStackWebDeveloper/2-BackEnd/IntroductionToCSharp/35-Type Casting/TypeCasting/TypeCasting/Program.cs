5d297c414352a9207148155910991ccd8dbbeaca
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-08 22:00:07 +0200

    Type Casting

--- FullStackWebDeveloper/2-BackEnd/IntroductionToCSharp/35-Type Casting/TypeCasting/TypeCasting/Program.cs
+++ FullStackWebDeveloper/2-BackEnd/IntroductionToCSharp/35-Type Casting/TypeCasting/TypeCasting/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Type Casting :-
+            //1-Implicit Casting
+            //2-Explicit Casting
+            //1. Implicit Casting (Widening) - Automatically done by C# when converting a smaller type to a larger type. No data loss.
+            int x = 5;
+            double y = x;
+            short m = 8;
+            // m = x; This is not allowed
+            /* 2.Explicit Casting(Narrowing) - Manually done by the programmer when converting a larger type to a smaller type.
+            Can cause data loss.*/
+            m = (short)x;
+            Console.WriteLine(m);
+            float A = 5.56789f;
+            double X = A;
+            A = (float)X;
+            int K = 5;
+            K = (int)A;
+            //Explicit Casting from float to integer can loss data type so here we use Explicit Casting.
+            Console.WriteLine(K);
         }
     }
 }
--- FullStackWebDeveloper/2-BackEnd/IntroductionToCSharp/35-Type Casting/TypeCasting/TypeCasting/obj/Debug/net8.0/TypeCasting.AssemblyInfo.cs
+++ FullStackWebDeveloper/2-BackEnd/IntroductionToCSharp/35-Type Casting/TypeCasting/TypeCasting/obj/Debug/net8.0/TypeCasting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypeCasting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b596b4cab2f76a94caa89d2e8c3114765e78cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d9be39f4a684bf0490c3662bbab1c4bbea535b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypeCasting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypeCasting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
