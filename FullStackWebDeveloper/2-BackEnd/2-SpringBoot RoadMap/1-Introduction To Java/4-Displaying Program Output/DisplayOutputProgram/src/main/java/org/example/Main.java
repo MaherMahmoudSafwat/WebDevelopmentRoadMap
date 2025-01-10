9a077f38b22096f09624b372d1e022221089bac0
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-10 10:01:44 +0200

    Escape Sequence

---
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/4-Displaying Program Output/DisplayOutputProgram/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Escape Sequences
+        //To print in a single line.
+        System.out.println("Hello, World!");
+        //To print in the same Line.
+        System.out.print("My Name is Maher.");
+        //\n To print a newline
+        System.out.print("I love Programming.\n");
+        System.out.println("I love Java.");
+        //\t to make tabs.
+        System.out.print("Weekends are \nFriday \tSaturday\n");
+        //\\ To print slash
+        System.out.println("he is \\ Maher.");
+        //\"To print Double Quotation and \' To print Single Quotation
+        System.out.println("I love Programming \"Java\"");
+        //\r Carriage To overwrite the old Sentence
+        System.out.print("I love Programming \"Java\"");
+        System.out.println("\rJava");
+    }
+}
