768fc96a9720ddefa412da2471366cbaaa9f9ada
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-01-12 20:57:22 +0200

    Update Main.java

--- FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/17-For Loop Statement/ForLoopStatement/src/Main.java
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/17-For Loop Statement/ForLoopStatement/src/Main.java
@@ -11,7 +11,7 @@ public class Main
         }
         for(int i = 10; i>=0; i--)
         {
-            System.out.println(i);
+            System.out.println(i); 
         }
         for(int i = 0; i<=10; i+=2)
         {
