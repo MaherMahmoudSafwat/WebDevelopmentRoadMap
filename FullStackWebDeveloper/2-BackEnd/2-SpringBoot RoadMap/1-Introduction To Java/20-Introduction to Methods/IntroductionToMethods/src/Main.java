ec28067998db4c2e5d3551ca21ce43958533b6cd
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-13 06:56:45 +0200

    Introduction To Methods

---
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/20-Introduction to Methods/IntroductionToMethods/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Function is used to divide and conquer and organize your code.
+        //Function is used to break your code in to small manageable units.
+        //Function and Method Both are the Same.
+
+        //Function Syntax and Function it is Signature.
+        /*
+          return value type FunctionName(Parameter-List (Parameter is Optional))
+          {
+           body of the code.
+          }
+         */
+        //Call the Function By it is Signature
+        /*DRY (don't repeat yourself) and this is done by the functions so if you want to fix an error you can fix it
+          only in one place inside the function.*/
+        //Functions Types :-
+        //1-Built-in Functions inside the Java Programming Language.
+        //2-Function you made it manually.
+        System.out.println(Math.sqrt(25));//Built-in Function.
+        System.out.println(Math.pow(2,3));//Built-in Function.
+        System.out.println(Math.abs(-10));//Built-in Function.
+        System.out.println(Math.floor(3.7));//Built-in Function.
+        System.out.println(Math.ceil(4.3));//Built-in Function.
+
+    }
+}
