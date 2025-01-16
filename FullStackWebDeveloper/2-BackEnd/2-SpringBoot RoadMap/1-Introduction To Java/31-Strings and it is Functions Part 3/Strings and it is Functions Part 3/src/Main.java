9d34f331c35c2155ae8c3ba8478f7fd716cf97bd
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-01-16 11:47:29 +0200

    Update Main.java

--- FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/31-Strings and it is Functions Part 3/Strings and it is Functions Part 3/src/Main.java
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/31-Strings and it is Functions Part 3/Strings and it is Functions Part 3/src/Main.java
@@ -1,44 +1,26 @@
 public class Main {
-    public static long IsSquare(long sq)
-    {
-        int Flag = 1;
-        do
-        {
-            Flag = 1;
-            sq++;
-            double x = Math.sqrt(sq);
-            String y = String.valueOf(x);
-            int Decimal = y.indexOf('.');
-            for(int i=Decimal+1;i<y.length();i++)
-            {
-                if(y.charAt(i)!='0')
-                {
-                        Flag = 2;
-                        break;
-                }
-            }
-        }while(Flag == 2);
-        return sq;
-    }
-    public static double countBits(long sq) {
-        double x = Math.sqrt(sq);
-        String y = String.valueOf(x);
-        int Flag = 1;
-        int Decimal = y.indexOf('.');
-        if (Decimal != -1) {
-            for (int i = Decimal + 1; i < y.length(); i++) {
-                if (y.charAt(i) != '0') {
-                    Flag = 2;
-                    break;
-                }
-            }
-            if (Flag == 2)
-                return -1;
-        }
-        return IsSquare(sq);
-    }
     public static void main(String[] args)
     {
-       System.out.println(countBits(121));
+         //To spilt an array of strings.
+        String S1 = "C++ / C / JAVA / C# / PYTHON";
+        String [] S2 = S1.split("/");
+        for(String i : S2)
+        {
+            System.out.println(i);
+        }
+        //To join an array of Strings
+        String S3 = String.join(S2,"");
+        System.out.println(S3);
+        //There is a Character class and it has it is own methods
+        Character A = 'A';
+        System.out.println(Character.toLowerCase(A));
+        //There is a String class and it has it is own Methods
+        String B = "B";
+        System.out.println(B.indexOf("B"));
+        //There is a Integer cxlass and it has it is own methods
+        Integer X = 51639;
+        System.out.println(Integer.max(X));
+        //Every Primitive data type has its own class and it is own method but you write the primitve data type completely and the first charcter of it is capital.
+        System.out.println(Double.max(123));
     }
 }
