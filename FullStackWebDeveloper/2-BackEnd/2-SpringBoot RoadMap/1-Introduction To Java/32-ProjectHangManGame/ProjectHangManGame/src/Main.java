630414aea6e316cedb1ad78bcc2d34b56a558393
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-17 15:11:08 +0200

    What is OOP ?

--- FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/32-ProjectHangManGame/ProjectHangManGame/src/Main.java
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/32-ProjectHangManGame/ProjectHangManGame/src/Main.java
@@ -1,49 +1,28 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-    public static String GetNewWord(String s, int Index)
-    {
-        String NewWord="";
-        if(Index==0)
-        {
-            NewWord += Character.toUpperCase(s.charAt(Index));
-            return NewWord;
-        }
-        else
-        {
-            for(int i=0; i<=Index; i++)
-            {
-                if(i==0)
-                {
-                    NewWord += Character.toUpperCase(s.charAt(Index));
-                    continue;
-                }
-                NewWord+=Character.toLowerCase(s.charAt(Index));
-            }
-        }
-        return NewWord;
+public class JavaFXExample extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a button and set an action on it
+        Button btn = new Button("Click Me");
+        btn.setOnAction(e -> System.out.println("Button Clicked!"));
+
+        // Set up the layout and scene
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 300, 200);
+
+        // Set up and show the stage (window)
+        primaryStage.setTitle("JavaFX Event-Driven Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-    public static String accum(String s)
-    {
-        String NewWord = "";
-        for(int i = 0; i<s.length(); i++)
-        {
-            if(i==(s.length()-1))
-            {
-                NewWord+=GetNewWord(s,i);
-                continue;
-            }
-            else
-            {
-                NewWord+=GetNewWord(s,i);
-                NewWord+='-';
-            }
-        }
-        return NewWord;
-    }
-    public static void main(String[] args)
-    {
-        System.out.println(accum("cwAt"));
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
