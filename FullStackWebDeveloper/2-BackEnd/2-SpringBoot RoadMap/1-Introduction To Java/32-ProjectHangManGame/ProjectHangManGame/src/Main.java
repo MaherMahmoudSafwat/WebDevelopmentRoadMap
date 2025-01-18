286ab69792ebb2ac064793a8613ae45659998c07
Maher Mahmoud Safwat <MaherMsWork2025@gmail.com>
2025-01-18 13:33:34 +0200

    Create your first class part 2

--- FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/32-ProjectHangManGame/ProjectHangManGame/src/Main.java
+++ FullStackWebDeveloper/2-BackEnd/2-SpringBoot RoadMap/1-Introduction To Java/32-ProjectHangManGame/ProjectHangManGame/src/Main.java
@@ -1,28 +1,9 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-public class JavaFXExample extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        // Create a button and set an action on it
-        Button btn = new Button("Click Me");
-        btn.setOnAction(e -> System.out.println("Button Clicked!"));
 
-        // Set up the layout and scene
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 300, 200);
+public class JavaFXExample
+{
+    public static void main(String[] args)
+    {
 
-        // Set up and show the stage (window)
-        primaryStage.setTitle("JavaFX Event-Driven Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
