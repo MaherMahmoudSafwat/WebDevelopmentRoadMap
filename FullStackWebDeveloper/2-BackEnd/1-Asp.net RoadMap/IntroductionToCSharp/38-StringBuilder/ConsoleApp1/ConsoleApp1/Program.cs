8d9f9fb9cd27c81f066937097201fe793a170545
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-01-28 16:08:53 +0200

    StringBuilder

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/IntroductionToCSharp/38-StringBuilder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name1 = "Maher";
+            Name1 = "Hello Maher";
+            //String here is immutable as you have reserved three spaces inside the memory.
+            Name1 += " , i love C#";
+            Console.WriteLine(Name1);
+
+            //So to overcome the immutable problem use the stringbuilder.
+            StringBuilder Name2 = new StringBuilder(4096);
+            Console.WriteLine(Name2.Length);
+            Console.WriteLine(Name2.Capacity);
+            Name2.Append("Hello ");
+            Name2.Append("World");
+            Console.WriteLine(Name2.ToString());
+            Console.WriteLine(Name2.Length);
+            Console.WriteLine(Name2.Capacity);
+            Name2.AppendLine(" I love programming");
+            Console.WriteLine(Name2.ToString());
+            Console.WriteLine(Name2.Length);
+            Console.WriteLine(Name2.Capacity);
+            Console.WriteLine(Name2.MaxCapacity);
+        }
+    }
+}
