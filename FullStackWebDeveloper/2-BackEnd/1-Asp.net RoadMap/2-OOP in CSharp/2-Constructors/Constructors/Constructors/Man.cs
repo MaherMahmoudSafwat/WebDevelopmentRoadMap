47841142039b3523197c0ff2762f7fdee9fa19ee
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-02-12 10:25:44 +0200

    Constructors

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/2-Constructors/Constructors/Constructors/Man.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Man
+    {
+        public Man(string Name) 
+        {
+            Console.WriteLine("Hello, World!");
+            this.Name = Name;
+        }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/2-Constructors/Constructors/Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using Constructors;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        /*
+         -The 'new Man("Maher")' creates a new 'Man' object in heap memory, 
+         - and 'man1' stores a reference (memory address) to that object. 
+         - The 'man1' variable itself is stored on the stack, but it points to 
+         - the actual object in the heap. The object is managed by the garbage collector 
+         - and will be cleaned up when no longer referenced.
+        */ 
+        Man man1 = new Man("Maher");
+        Console.WriteLine(man1.Name);
+    }
+}
