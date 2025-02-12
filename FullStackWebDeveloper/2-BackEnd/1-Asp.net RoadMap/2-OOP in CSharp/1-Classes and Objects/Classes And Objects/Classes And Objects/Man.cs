7c6202a89cf343f17efb61745518716177bf3ae4
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-02-12 02:12:06 +0200

    Classes and Objects

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/1-Classes and Objects/Classes And Objects/Classes And Objects/Man.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Objects
+{
+    internal class Man
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/1-Classes and Objects/Classes And Objects/Classes And Objects/Program.cs
@@ -0,0 +1,29 @@
+﻿using Classes_And_Objects;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        //All classes in csharp extends and inherits from class object indirectly or directly.
+        Man man = new Man();
+        object obj = new object();
+        dynamic x = 0;
+        x = "s";
+        man.Name = "Maher";
+        man.Address = "Faisal";
+        man.PhoneNumber = "12345";
+        Console.WriteLine(man.Name);
+        Man[] men = new Man[3];
+        men[0] = new Man();
+        men[1] = new Man();
+        men[2] = new Man();
+        men[0].Name = "Yousef";
+        men[1].Name = "Ahmed";
+        men[2].Name = "Mostafa";
+        foreach(Man Men1 in men)
+        {
+            Console.WriteLine(Men1.Name);
+        }
+        men[0] = man;
+    }
+}
