470ff7af5c1c2e76dff99640c2fe326fdab72b53
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-02-12 11:26:41 +0200

    Properties

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/3-Properties/Properties/Properties/Man.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Man
+    {
+        //Constructor is the only place where you can assign a value to the Read-Only Property.  
+        public Man(int id) 
+        {
+            ID = id;
+        }
+        //public string Name { get; set; } //Auto-Implemented Property.  
+        /*public void setName(string name)
+        {
+             Name = name;
+        }
+        public string getName()
+        {
+            return Name;
+        }*/
+
+        //Backing Field.
+        private string _name;
+        public string Name
+        {
+            set
+            {
+                if (value.Length > 5)
+                    return;
+                _name = value;
+            }
+            get
+            {
+                return _name;
+            }
+        }
+        //Read-Only Property
+        public int ID { get;}
+        /* public void setID(int id)
+        {
+            ID = id;
+        }*///Not Allowed because ID is Read-Only Property. 
+        public string Address { get; init; }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/3-Properties/Properties/Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using Properties;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        Man man = new Man(13) { Address = "Faisal"};
+        man.Name = "Maher";
+        Console.WriteLine(man.Name);
+        man.Name = "Laptop";
+        Console.WriteLine(man.Name);
+        //man.ID = 1; Not allowed because it is read-only property.
+        Console.WriteLine(man.ID);
+        //man.Address = "Faisal";You can't assign a value here it is not allowed because it has an init accessor. 
+        Console.WriteLine(man.Address);
+    }
+}
