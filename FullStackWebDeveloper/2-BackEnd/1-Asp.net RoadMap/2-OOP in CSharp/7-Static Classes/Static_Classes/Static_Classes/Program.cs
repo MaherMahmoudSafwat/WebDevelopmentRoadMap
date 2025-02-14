ce17588dd1c0b49646c32e26ccae5c0841409320
Maher <MaherMsSoftware2001@gmail.com>
2025-02-14 18:57:01 +0200

    Constants and readonly

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/7-Static Classes/Static_Classes/Static_Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Static_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Student = new Student1();
+            Student.PrintName();
+            //Student.PrintYpourSpecification();Not allowed because PrintYpourSpecification is a static method.
+            Student1.PrintYpourSpecification();
+            Console.WriteLine(Student2.Name());
+            Student2.Name();
+            Student2.Name();
+            Student2.Name();
+            Student2.Name();
+            Student2.Name();
+            Student2.Name();
+            Student2.Name();
+            Console.WriteLine(Student2.Counter);//8
+        }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/7-Static Classes/Static_Classes/Static_Classes/Student1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Classes
+{
+    //Non-static class can have static members or methods.
+    //Static class must all of it is members and methjods become static.
+    //Any static methods use only static variables but Non-static methods can have static members. 
+    class Student1
+    {
+        //Access Modifiers are not allowed with static keyword in Constructors signature declaration. 
+        //Static constructor is only called one time in the propgram.
+        static Student1()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static string Name = "Maher";
+        public string Address = "Faisal";
+        public void PrintName()
+        {
+            Console.WriteLine("Hello Maher");
+        }
+        //Static members or methods can be called directly by class without making any object from it.
+        //static is in class level not object level.
+        public static void PrintYpourSpecification()
+        {
+            Console.WriteLine("Software Engineering and Back-end Developer with C#");
+        }
+        public void CallName1()
+        {
+            Console.WriteLine(Name);//This is Allowed
+        }
+        public static void CallName2()
+        {
+            //Console.WriteLine(Address);This is not allowed.
+        }
+    }
+    public static class Student2
+    {
+        public static int Counter = 0;
+        static Student2()
+        {
+            Console.WriteLine("Constructor is called.");
+        }
+        public static string Name()
+        {
+            Counter++;
+            return "Hello World";
+        }
+        /* public string Address()
+          {
+              return "Faisal";
+          }
+        *///This is not allowed.
+    }
+}
