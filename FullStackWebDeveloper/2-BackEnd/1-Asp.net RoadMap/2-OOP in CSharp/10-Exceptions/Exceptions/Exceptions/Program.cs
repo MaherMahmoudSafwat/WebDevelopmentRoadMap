cbe42e5652ed5df89378757153bd58d5c0fd3e81
Maher <MaherMsSoftware2001@gmail.com>
2025-02-15 21:34:29 +0200

    Exceptions

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/10-Exceptions/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.WriteLine("Hello, World!");Compile time error Runtime.
+            //All Exceptions in C# inherits from a class called Exception. 
+            //The try can has one or more catch but it must has a catch or finally or both of them.
+            try
+            {
+                int x = int.Parse("Test Error Code");
+                Console.WriteLine(Divide(10, 2));
+                Console.WriteLine(Divide(5, 0));
+                Console.WriteLine(Divide(15, 3));
+            }
+            //The catch statement is where it handles the Exception error.
+            catch (DivideByZeroException ex)//Paramerters of catch are optionally you can have a catch without Parameters but it must be at the end.
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.HelpLink);
+            }
+            catch
+            {
+                Console.WriteLine("Catch Error Exception");
+            }
+            //The finally block is an optional block that will be exceuted whether the try is executed or catch is executed. 
+            finally
+            {
+                Console.WriteLine("This is a finally block");
+            }
+            Console.WriteLine("Hello C#.");
+
+
+            Console.WriteLine("Please enter a five string character.");
+            string z = Console.ReadLine();
+            if(z.Length < 5)
+            {
+                throw new Exception("Length must be greater than five characters.");
+            }
+        }
+        static int Divide(int Number1, int Divisor) 
+        {
+            return Number1 / Divisor;
+        }
+    }
+}
