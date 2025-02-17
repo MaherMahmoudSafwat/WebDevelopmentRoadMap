ce76a4663bc6e0f292b79f2f19fe7386d648d859
Maher <MaherMsSoftware2001@gmail.com>
2025-02-17 14:01:25 +0200

    Random Numbers Generators

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/12-Flags Enums/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static string AlphabetWar(string fight)
+        {
+            bool IsAllStars = true;
+            for(int i=0;i<fight.Length;i++)
+            {
+                if (fight[i] == '*')
+                    continue;
+                IsAllStars = false;
+                break;
+            }
+            if (IsAllStars)
+                return "Let's fight again!";
+            StringBuilder NewString = new StringBuilder();
+            for(int i=0;i<fight.Length;i++) //zz*zzs
+            {
+                if(fight.Length==1)
+                {
+                    if (fight[i] != '*')
+                        NewString.Append(fight[i]);
+                    continue;
+                }
+                if(i==fight.Length-1)
+                {
+                    if (fight[i - 1] != '*' && fight[i] != '*')
+                        NewString.Append(fight[i]);
+                    break;
+                }
+                if (fight[i] == '*')
+                {
+                    i++;
+                    continue;
+                }
+                else if (fight[i + 1] == '*')
+                    continue;
+                else if (i != 0)
+                {
+                    if (fight[i - 1] != '*')
+                        continue;
+                }
+
+                NewString.Append(fight[i]);
+            }
+            int LeftSide = 0, RightSide = 0; 
+            for (int i = 0; i<NewString.Length;i++)
+            {
+                if (NewString[i]=='z')
+                {
+
+                RightSide++;
+
+                }
+                else if (NewString[i] == 'd')
+                {
+                    RightSide+=2;
+
+                }
+                else if (NewString[i] == 'q')
+                {
+                    RightSide+=3;
+
+                }
+                else if (NewString[i] == 'm')
+                {
+                    RightSide+= 4;
+                }
+                else if (NewString[i] == 's')
+                {
+                    LeftSide++;
+                }
+                else if (NewString[i] == 'b')
+                {
+                    LeftSide += 2;
+                }
+                else if (NewString[i] == 'p')
+                {
+                    LeftSide += 3;
+                }
+                else if (NewString[i] == 'w')
+                {
+                    LeftSide += 4;
+                }
+
+            }
+            if (LeftSide > RightSide)
+                return "Left side wins!";
+            else if (RightSide > LeftSide)
+                return "Right side wins!";
+            return "Let's fight again!";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlphabetWar("zz*zzs"));
+            //1-qkwtuh*sn**qmpgbp***
+            //2-qkwtun**qmpgbp***
+            //3-qkwtuqmpgbp***
+            //4-qkwtuqmpg
+        }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/13-Random Values Generator/Random App Generator/Random App Generator/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Random_App_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please choose [1]Random Numbers Generator\t[2]Random Strings Generator");
+                string UserChoice = Console.ReadLine();
+                if (UserChoice == "1")
+                {
+                    Console.Write("Enter min number :- ");
+                    int Number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter max number :- ");
+                    int Number2 = int.Parse(Console.ReadLine());
+                    RandomNumberGenerator(Number1, Number2);
+                }
+                else if (UserChoice == "2")
+                {
+                    Console.Write("Enter length of the string :- ");
+                    int Length = int.Parse(Console.ReadLine());
+                    RandomStringGenerator(Length);
+                }
+            } 
+        }
+        public static void RandomNumberGenerator(int num1, int num2)
+        {
+            Random Rnd = new Random();
+            var RandomNumber = Rnd.Next(num1, num2);
+            Console.WriteLine(RandomNumber);
+        }
+        public static void UserChoice(out bool CapitalLetters,out bool SmallLetters,out bool IncludeNumbers, out bool IncludeSymbols)
+        {
+            Console.Write("Would you like to include capital letters ? yes/no ?");
+            CapitalLetters = (Console.ReadLine().ToUpper() == "YES") ? true : false;
+            Console.Write("Would you like to include small letters ? yes/no ?");
+            SmallLetters = (Console.ReadLine().ToUpper() == "YES") ? true : false;
+            Console.Write("Would you like to include numbers ? yes/no ?");
+            IncludeNumbers = (Console.ReadLine().ToUpper() == "YES") ? true : false;
+            Console.Write("Would you like to include symbols ? yes/no ?");
+            IncludeSymbols = (Console.ReadLine().ToUpper() == "YES") ? true : false;
+        }
+        public static void RandomStringGenerator(int Length)
+        {
+            string RandomString = "";
+            Random Rnd = new Random();
+            int RandomNumber = 0;
+            int RandomNumbers2 = 0;
+            bool CapitalLetters, SmallLetters, IncludeNumbers, IncludeSymbols;
+            UserChoice(out CapitalLetters,out SmallLetters,out IncludeNumbers,out IncludeSymbols);
+            while (RandomString.Length < Length)
+            {
+                RandomNumber = Rnd.Next(1, 5);
+                if(RandomNumber == 1)
+                {
+                    if(CapitalLetters)
+                    {
+                        RandomString += (char)(Rnd.Next(65, 91));
+                    }
+                }
+                else if(RandomNumber == 2)
+                {
+                    if(SmallLetters)
+                    {
+                        RandomString += (char)(Rnd.Next(97, 123));
+                    }
+                }
+                else if(RandomNumber == 3)
+                {
+                    if(IncludeNumbers)
+                    {
+                        RandomString += (char)(Rnd.Next(48, 58));
+                    }
+                }
+                else if(RandomNumber == 4)
+                {
+                    if(IncludeSymbols)
+                    {
+                        RandomNumbers2 = Rnd.Next(1, 5);
+                        if (RandomNumbers2 == 1)
+                            RandomString += (char)(Rnd.Next(32, 48));
+                        else if (RandomNumbers2 == 2)
+                            RandomString += (char)(Rnd.Next(58, 65));
+                        else if (RandomNumbers2 == 3)
+                            RandomString += (char)(Rnd.Next(91, 97));
+                        else
+                            RandomString += (char)(Rnd.Next(123, 127)); 
+                    }
+                }
+            }
+            Console.WriteLine(RandomString);
+        }
+    }
+}
