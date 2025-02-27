fc91c28e53d5d92727f102b613cf2ff1d1b24bab
Maher <MaherMsSoftware2001@gmail.com>
2025-02-27 21:54:16 +0200

    Inheritance

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/29-Inheritance/Inheritance/Inheritance/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        //The point of Encapsulation is to avoid invalid inputs.
+        /* 
+         Encapsulation - Explanation & Purpose
+         🔹 Encapsulation is one of the core principles of Object-Oriented Programming(OOP). It refers to the practice of hiding the internal details of an object and only exposing controlled access through methods.
+
+         🔹 It helps in protecting data from direct modification, ensuring that only allowed operations are performed on it.
+
+         🔹 The main goal of encapsulation is to achieve data security, maintainability, and modularity in a program.
+
+         🔹 It allows developers to enforce rules and validation when accessing or modifying data, preventing unintended or harmful changes.
+
+         🔹 Encapsulation improves code organization by grouping related data (attributes) and behaviors (methods) inside a single unit (class).
+         */
+        //The point of Encapsulation is to avoid invalid inputs.
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public void SetName(string FirstName, string LastName)
+        {
+            if (string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName))
+                throw new Exception("Invalid Name");
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        //We used a private set to make the method be called by the method SetDateBirth and not from the auto-implemented method.
+        public DateOnly BirthDate { private set; get; }
+        /* {
+             get
+             {
+                 return BirthDate;
+             }
+             set
+             {
+                 this.BirthDate = BirthDate;
+             }
+         }*///This is Allowed.
+        public int BasicSalary { set; get; }
+        public int TaxPercentage { set; get; }
+        public void SetDateBirth(DateOnly BirthDate)
+        {
+            if (BirthDate < new DateOnly(1986, 7, 1))
+                throw new ArgumentException("Invalid date");
+            this.BirthDate = BirthDate;
+        }
+    }
+}
---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/29-Inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee = new Employee();
+            Employee.SetName("Mohammed", "Ahmed");
+            Employee.SetDateBirth(new DateOnly(2000, 1, 1));
+            Employee.BasicSalary = -5000;
+            Employee.TaxPercentage = 140;
+
+            PrintPersonDetails(Employee);
+            Console.WriteLine("----------------------");
+            var Applicant = new Applicant();
+            Applicant.SetName("Mohammed", "Abbas");
+            Applicant.SetDateBirth(new DateOnly(2004, 5, 20));
+            PrintPersonDetails(Applicant);
+            Person Person = Employee;
+            Person = Applicant; 
+
+            Console.WriteLine($"Basic Salary:\t{Employee.BasicSalary}0:00");
+            Console.WriteLine($"Tax Salary:\t{Employee.TaxPercentage}%");
+            Console.ReadKey();
+        }
+        static void PrintPersonDetails(Person Person)
+        {
+            Console.WriteLine($"Full Name:\t{Person.FirstName} {Person.LastName}");
+            Console.WriteLine($"Birth Date:\t{Person.BirthDate}");
+        }
+    }
+}
