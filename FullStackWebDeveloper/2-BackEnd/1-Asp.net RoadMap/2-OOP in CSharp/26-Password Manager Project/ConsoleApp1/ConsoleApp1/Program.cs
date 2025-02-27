9af594834307b8378dbd8fe4534790990715c85e
Maher <MaherMsSoftware2001@gmail.com>
2025-02-27 13:11:15 +0200

    Intro to OOP

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/26-Password Manager Project/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int[] Array2 = new int[nums.Length];
+            int j = 0;
+            for(int i = 0 ;i<nums.Length ;i++)
+            {
+                if (nums[i]!=0)
+                {
+                    Array2[j] = nums[i];
+                    j++;
+                }
+            }
+            for(;j<nums.Length;j++)
+            {
+                Array2[j] = 0;
+            }
+            nums = Array2;
+        }
+        static void Main(string[] args)
+        {
+            int[] Array123 = { 0, 1, 0, 3, 12 };
+            MoveZeroes(Array123);
+        }
+    }
+}
