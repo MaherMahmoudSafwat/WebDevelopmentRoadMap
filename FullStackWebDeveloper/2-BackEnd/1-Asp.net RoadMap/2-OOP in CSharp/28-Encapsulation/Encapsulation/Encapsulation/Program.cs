6dd3e996624e067c9c0ca0aa0af18834819fff67
Maher <MaherMsSoftware2001@gmail.com>
2025-02-27 15:01:49 +0200

    Encapsulation

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/2-OOP in CSharp/28-Encapsulation/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var HR = new HR();
+            HR.SetName("Mohammed", "Ahmed");
+            HR.SetDateBirth(new DateOnly(2000, 1, 1));
+            HR.BasicSalary = -5000;
+            HR.TaxSalary = 140;
+            Console.WriteLine($"Full Name:\t{HR.FirstName} {HR.LastName}");
+            Console.WriteLine($"Birth Date:\t{HR.BirthDate}");
+            Console.WriteLine($"Basic Salary:\t{HR.BasicSalary}0:00");
+            Console.WriteLine($"Tax Salary:\t{HR.TaxSalary}%");
+            Console.ReadKey();
+        }
+    }
+}
