67a01ecd12d4d42e0f762bf6e5a512c8cf5e04ea
Maher <MaherMsSoftware2001@gmail.com>
2025-03-02 11:15:26 +0200

    Recursion

---
+++ FullStackWebDeveloper/2-BackEnd/1-Asp.net RoadMap/3-Advanced CSharp/2-Recursion/Recursion/Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static long CalculateDirectorySize(string dirpath)
+        {
+            long Size = 0;
+            foreach(var FileName in Directory.GetFiles(dirpath))
+            {
+                Size += new FileInfo(FileName).Length;
+            }
+            foreach(var DirectoryName in Directory.GetDirectories(dirpath))
+            {
+                Size += CalculateDirectorySize(DirectoryName);
+            }
+            return Size;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFactorial(5)); 
+            Console.WriteLine(CalculateDirectorySize("C:\\Users\\Maher\\Desktop\\WebDevelopmentRoadMap\\FullStackWebDeveloper\\2-BackEnd")/1024);
+        }
+        static int CalculateFactorial(int number)
+        {
+            if (number <= 1)
+                return number;
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
